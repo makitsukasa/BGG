96abdbc5cbffc0bc783c0d4e9f8703a8ec829788
Unknown <allium.fistulosum.39@gmail.com>
2019-02-28 21:16:46 +0900

    refactor

--- jgg.py
+++ jgg.py
@@ -1,51 +1,63 @@
 import numpy as np
 from individual import Individual
 
+# 用語は 遺伝的アルゴリズムにおける世代交代モデルの提案と評価(佐藤，97)による
+
 class JGG:
-	def __init__(self, n, problem):
+	def __init__(self, n, npop, npar, nchi, problem):
 		self.n = n
+		self.npar = npar
+		self.nchi = nchi
 		self.problem = problem
-
-	def initialize(self, npop):
 		self.population = [Individual(self.n) for i in range(npop)]
-		return self.population
+		self.history = []
+		self.history.append(np.average([self.problem(i.gene) for i in self.population]))
+		# self.history.append(np.amin([self.problem(i.gene) for i in self.population]))
 
-	def reproduction(self, npar):
+	def selection_for_reproduction(self):
 		np.random.shuffle(self.population)
-		self.parents = self.population[:npar]
-		return self.parents
+		parents = self.population[:self.npar]
+		self.population = self.population[self.npar:]
+		return parents
 
-	def crossover(self, nchi):
-		mu = len(self.parents)
-		mean = np.mean(np.array([parent.gene for parent in self.parents]), axis = 0)
-		self.children = [Individual(self.n) for i in range(nchi)]
-		for child in self.children:
+	def crossover(self, parents):
+		mu = len(parents)
+		mean = np.mean(np.array([parent.gene for parent in parents]), axis = 0)
+		children = [Individual(self.n) for i in range(self.nchi)]
+		for child in children:
 			epsilon = np.random.normal(0.0, np.sqrt(1 / (mu - 1)), mu)
 			child.gene = mean + np.sum(
-				[epsilon[i] * (self.parents[i].gene - mean) for i in range(mu)], axis = 0)
-		return self.children
+				[epsilon[i] * (parents[i].gene - mean) for i in range(mu)], axis = 0)
+		return children
 
-	def survival_selection(self, npar):
-		self.evaluate(self.children)
-		self.children.sort(key = lambda child: child.fitness)
-		self.population[:npar] = self.children[:npar]
-		return self.population
+	def selection_for_survival(self, children):
+		children.sort(key = lambda child: child.fitness)
+		return children[:self.npar]
 
 	def evaluate(self, pop):
-		for s in pop:
-			s.fitness = self.problem(s.gene)
+		for individual in pop:
+			individual.fitness = self.problem(individual.gene)
+		self.history.append(np.average([i.fitness for i in pop]))
+		return pop
+
+	def alternation(self):
+		parents = self.selection_for_reproduction()
+		children = self.crossover(parents)
+		self.evaluate(children)
+		elites = self.selection_for_survival(children)
+		self.population.extend(elites)
 
 	def get_best_evaluation_value(self):
 		self.population.sort(key = lambda s: s.fitness if s.fitness else np.inf)
 		return self.population[0].fitness, self.population[0].gene
 
 if __name__ == '__main__':
-	n = 5
-	ga = JGG(n, lambda x: np.sum(x**2))
+	n = 20
+	ga = JGG(n, 6 * n, n + 1, 6 * n, lambda x: np.sum((x * 10.24 - 5.12) ** 2))
 
-	ga.initialize(14 * n)
-	for i in range(300):
-		ga.reproduction(n + 1)
-		ga.crossover(5 * n)
-		ga.survival_selection(n + 1)
+	for i in range(230):
+		ga.alternation()
 	print(ga.get_best_evaluation_value())
+
+	for h in ga.history:
+		print(h)
