ba10ae716482bd71f6de3d673bed2f517a952212
Unknown <allium.fistulosum.39@gmail.com>
2019-03-03 10:34:27 +0900

    bench functions

--- main.py
+++ main.py
@@ -9,24 +9,42 @@ from problem.frontier.schaffer import schaffer
 from problem.frontier.rastrigin import rastrigin
 
 n = 20
-for problem in [sphere, ktablet, bohachevsky, ackley, schaffer, rastrigin]:
-# for problem in [sphere]:
+
+problems = [
+	{"name" : "sphere",      "func" : sphere,      "npop" :  6 * n, "nchi" : 6 * n},
+	# {"name" : "ellipsoid",   "func" : ellipsoid,   "npop" :  6 * n, "nchi" : 6 * n},
+	{"name" : "k-tablet",    "func" : ktablet,     "npop" :  8 * n, "nchi" : 6 * n},
+	# {"name" : "rosenbrock",  "func" : rosenbrock,  "npop" : 15 * n, "nchi" : 8 * n},
+	{"name" : "bohachevsky", "func" : bohachevsky, "npop" :  6 * n, "nchi" : 6 * n},
+	{"name" : "ackley",      "func" : ackley,      "npop" :  8 * n, "nchi" : 6 * n},
+	{"name" : "schaffer",    "func" : schaffer,    "npop" : 10 * n, "nchi" : 8 * n},
+	{"name" : "rastrigin",   "func" : rastrigin,   "npop" : 24 * n, "nchi" : 8 * n},
+]
+
+for problem in problems:
+	func = problem["func"]
+	name = problem["name"]
+	npop = problem["npop"]
+	nchi = problem["nchi"]
 	jgg_counts = []
 	bgg_counts = []
 
+	print(name)
+
 	for i in range(30):
-		jgg = JGG(n, 6 * n, n + 1, 6 * n, sphere)
-		result = jgg.until(1e-7, 300000)
+		jgg = JGG(n, npop, n + 1, nchi, func)
+		result = jgg.until(1e-7, 300000 // (nchi))
 		if result:
 			jgg_counts.append(len(jgg.history))
 		else:
-			print("jgg failed", problem.__name__)
+			print("jgg failed")
 
-		bgg = BGG(n, 6 * n, n + 1, 6 * n, sphere)
-		result = bgg.until(1e-7, 300000)
+		bgg = BGG(n, npop, n + 1, nchi, func)
+		result = bgg.until(1e-7, 300000 // (nchi))
 		if result:
 			bgg_counts.append(len(bgg.history))
+		else:
+			print("bgg failed")
 
-	print(problem.__name__)
 	print("jgg:", np.average(jgg_counts))
 	print("bgg:", np.average(bgg_counts))
