6293a132016fb3f8184938cad4f5280739ac2f3e
Unknown <allium.fistulosum.39@gmail.com>
2019-03-29 14:26:15 +0900

    more linear b

--- main.py
+++ main.py
@@ -58,6 +58,50 @@ for problem in problems:
 					f.write("{0},{1}\n".format(c, v))
 				f.close()
 
+		method_name = "BGG(子数可変 一部優秀 b=x÷300)"
+		bgg = BGG(n, npop, n + 1, nchi, func)
+		bgg.get_nchi = bgg.get_nchi_barotmetic
+		bgg.select_for_reproduction = bgg.select_for_reproduction_partitioned
+		bgg.barometer = bgg.barometer_linear(300, 0)
+		result = bgg.until(1e-7, max_eval_count)
+		if result:
+			if method_name in eval_counts:
+				eval_counts[method_name].append(bgg.eval_count)
+			else:
+				eval_counts[method_name] = [bgg.eval_count]
+		else:
+			print(method_name, "failed")
+
+		if SAVE_HISTORY_CSV:
+			filename = "benchmark/{0}_{1}.csv"\
+				.format(method_name, name)
+			with open(filename, "w") as f:
+				for c, v in bgg.history.items():
+					f.write("{0},{1}\n".format(c, v))
+				f.close()
+
+		method_name = "BGG(子数可変 親候補限 b=x÷300)"
+		bgg = BGG(n, npop, n + 1, nchi, func)
+		bgg.get_nchi = bgg.get_nchi_barotmetic
+		bgg.select_for_reproduction = bgg.select_for_reproduction_restricted
+		bgg.barometer = bgg.barometer_linear(300, 0)
+		result = bgg.until(1e-7, max_eval_count)
+		if result:
+			if method_name in eval_counts:
+				eval_counts[method_name].append(bgg.eval_count)
+			else:
+				eval_counts[method_name] = [bgg.eval_count]
+		else:
+			print(method_name, "failed")
+
+		if SAVE_HISTORY_CSV:
+			filename = "benchmark/{0}_{1}.csv"\
+				.format(method_name, name)
+			with open(filename, "w") as f:
+				for c, v in bgg.history.items():
+					f.write("{0},{1}\n".format(c, v))
+				f.close()
+
 		method_name = "BGG(子数可変 一部優秀 b=x÷600)"
 		bgg = BGG(n, npop, n + 1, nchi, func)
 		bgg.get_nchi = bgg.get_nchi_barotmetic
@@ -102,11 +146,11 @@ for problem in problems:
 					f.write("{0},{1}\n".format(c, v))
 				f.close()
 
-		method_name = "BGG(子数可変 一部優秀 b=x÷600-1)"
+		method_name = "BGG(子数可変 一部優秀 b=x÷900)"
 		bgg = BGG(n, npop, n + 1, nchi, func)
 		bgg.get_nchi = bgg.get_nchi_barotmetic
 		bgg.select_for_reproduction = bgg.select_for_reproduction_partitioned
-		bgg.barometer = bgg.barometer_linear(600, -1)
+		bgg.barometer = bgg.barometer_linear(900, 0)
 		result = bgg.until(1e-7, max_eval_count)
 		if result:
 			if method_name in eval_counts:
@@ -124,11 +168,11 @@ for problem in problems:
 					f.write("{0},{1}\n".format(c, v))
 				f.close()
 
-		method_name = "BGG(子数可変 親候補限 b=x÷600-1)"
+		method_name = "BGG(子数可変 親候補限 b=x÷900)"
 		bgg = BGG(n, npop, n + 1, nchi, func)
 		bgg.get_nchi = bgg.get_nchi_barotmetic
 		bgg.select_for_reproduction = bgg.select_for_reproduction_restricted
-		bgg.barometer = bgg.barometer_linear(600, -1)
+		bgg.barometer = bgg.barometer_linear(900, 0)
 		result = bgg.until(1e-7, max_eval_count)
 		if result:
 			if method_name in eval_counts:
@@ -190,11 +234,11 @@ for problem in problems:
 					f.write("{0},{1}\n".format(c, v))
 				f.close()
 
-		method_name = "BGG(子数可変 一部優秀 b=x÷1200-1)"
+		method_name = "BGG(子数可変 一部優秀 b=x÷1200+0.5)"
 		bgg = BGG(n, npop, n + 1, nchi, func)
 		bgg.get_nchi = bgg.get_nchi_barotmetic
 		bgg.select_for_reproduction = bgg.select_for_reproduction_partitioned
-		bgg.barometer = bgg.barometer_linear(1200, -1)
+		bgg.barometer = bgg.barometer_linear(1200, 0.5)
 		result = bgg.until(1e-7, max_eval_count)
 		if result:
 			if method_name in eval_counts:
@@ -212,11 +256,11 @@ for problem in problems:
 					f.write("{0},{1}\n".format(c, v))
 				f.close()
 
-		method_name = "BGG(子数可変 親候補限 b=x÷1200-1)"
+		method_name = "BGG(子数可変 親候補限 b=x÷1200+0.5)"
 		bgg = BGG(n, npop, n + 1, nchi, func)
 		bgg.get_nchi = bgg.get_nchi_barotmetic
 		bgg.select_for_reproduction = bgg.select_for_reproduction_restricted
-		bgg.barometer = bgg.barometer_linear(1200, -1)
+		bgg.barometer = bgg.barometer_linear(1200, 0.5)
 		result = bgg.until(1e-7, max_eval_count)
 		if result:
 			if method_name in eval_counts:
@@ -235,11 +279,11 @@ for problem in problems:
 				f.close()
 
 
-		method_name = "BGG(子数可変 一部優秀 b=x÷2400)"
+		method_name = "BGG(子数可変 一部優秀 b=x÷2400+0.5)"
 		bgg = BGG(n, npop, n + 1, nchi, func)
 		bgg.get_nchi = bgg.get_nchi_barotmetic
 		bgg.select_for_reproduction = bgg.select_for_reproduction_partitioned
-		bgg.barometer = bgg.barometer_linear(2400, 0)
+		bgg.barometer = bgg.barometer_linear(2400, 0.5)
 		result = bgg.until(1e-7, max_eval_count)
 		if result:
 			if method_name in eval_counts:
@@ -257,11 +301,11 @@ for problem in problems:
 					f.write("{0},{1}\n".format(c, v))
 				f.close()
 
-		method_name = "BGG(子数可変 親候補限 b=x÷2400)"
+		method_name = "BGG(子数可変 親候補限 b=x÷2400+0.5)"
 		bgg = BGG(n, npop, n + 1, nchi, func)
 		bgg.get_nchi = bgg.get_nchi_barotmetic
 		bgg.select_for_reproduction = bgg.select_for_reproduction_restricted
-		bgg.barometer = bgg.barometer_linear(2400, 0)
+		bgg.barometer = bgg.barometer_linear(2400, 0.5)
 		result = bgg.until(1e-7, max_eval_count)
 		if result:
 			if method_name in eval_counts:
