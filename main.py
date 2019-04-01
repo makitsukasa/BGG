2678b5ab6abaf67e80d40250fd6087bf857777dd
Unknown <allium.fistulosum.39@gmail.com>
2019-04-02 03:00:33 +0900

    benchmark

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ from problem.frontier.rastrigin   import rastrigin
 
 warnings.simplefilter("error", RuntimeWarning)
 
-SAVE_HISTORY_CSV = True
-SAVE_COUNTS_CSV = False
+SAVE_HISTORY_CSV = False
+SAVE_COUNTS_CSV = True
 
 n = 20
 
 problems = [
-	{"name" : "sphere",      "func" : sphere,      "npop" :  6 * n, "nchi" : 6 * n},
+	# {"name" : "sphere",      "func" : sphere,      "npop" :  6 * n, "nchi" : 6 * n},
 	# {"name" : "k-tablet",    "func" : ktablet,     "npop" : 10 * n, "nchi" : 6 * n},
 	# {"name" : "bohachevsky", "func" : bohachevsky, "npop" :  8 * n, "nchi" : 6 * n},
 	# {"name" : "ackley",      "func" : ackley,      "npop" :  8 * n, "nchi" : 6 * n},
 	# {"name" : "schaffer",    "func" : schaffer,    "npop" : 11 * n, "nchi" : 8 * n},
-	# {"name" : "rastrigin",   "func" : rastrigin,   "npop" : 24 * n, "nchi" : 8 * n},
+	{"name" : "rastrigin",   "func" : rastrigin,   "npop" : 24 * n, "nchi" : 8 * n},
 ]
 
 datestr = "{0:%Y-%m-%d_%H-%M-%S}".format(datetime.datetime.now())
@@ -35,7 +35,7 @@ for problem in problems:
 	nchi = problem["nchi"]
 	eval_counts = {}
 	max_eval_count = 300000
-	loop_count = 1
+	loop_count = 1000
 
 	print(name, loop_count, flush = True)
 
