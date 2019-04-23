e81cd4a2f39931b3de9d8166bf0d3a49acd40459
Unknown <allium.fistulosum.39@gmail.com>
2019-04-24 00:20:22 +0900

    bugfix variable name

--- main.py
+++ main.py
@@ -48,9 +48,9 @@ for problem in problems:
 		result = bgg.until(1e-7, max_eval_count)
 		if result:
 			if method_name in eval_counts:
-				eval_counts[method_name].append(jgg.eval_count)
+				eval_counts[method_name].append(bgg.eval_count)
 			else:
-				eval_counts[method_name] = [jgg.eval_count]
+				eval_counts[method_name] = [bgg.eval_count]
 		else:
 			print(method_name, "failed")
 		if SAVE_HISTORY_CSV:
