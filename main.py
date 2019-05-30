42a19ddd1257d62a7c44084f2c28c0ec9fbb6bcc
makitsukasa <allium.fistulosum.39@gmail.com>
2019-05-30 15:48:16 +0900

    plot errorbar

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from problem.frontier.rastrigin   import rastrigin
 
 warnings.simplefilter("error", RuntimeWarning)
 
-def save(system, result, method_name, problem_name):
+def save(system, result, method_name, problem_name, index):
 	if result:
 		if method_name in eval_counts:
 			eval_counts[method_name].append(system.eval_count)
@@ -22,33 +22,33 @@ def save(system, result, method_name, problem_name):
 	else:
 		print(method_name, "failed")
 	if SAVE_HISTORY_CSV:
-		filename = "benchmark/評価値_{0}_{1}.csv"\
-			.format(method_name, problem_name)
+		filename = "benchmark/評価値_{0}_{1}_{2}.csv"\
+			.format(method_name, problem_name, index)
 		with open(filename, "w") as f:
 			for c, v in system.history.items():
 				f.write("{0},{1}\n".format(c, v))
 			f.close()
 	if SAVE_DISTANCE_CSV:
-		filename = "benchmark/距離_{0}_{1}.csv"\
-			.format(method_name, problem_name)
+		filename = "benchmark/距離_{0}_{1}_{2}.csv"\
+			.format(method_name, problem_name, index)
 		with open(filename, "w") as f:
 			for c, v in system.mean_of_distance_history.items():
 				f.write("{0},{1}\n".format(c, v))
 			f.close()
 
-SAVE_HISTORY_CSV = False
-SAVE_DISTANCE_CSV = False
-SAVE_COUNTS_CSV = True
+SAVE_HISTORY_CSV = True
+SAVE_DISTANCE_CSV = True
+SAVE_COUNTS_CSV = False
 
 n = 20
 
 problems = [
 	{"name" : "sphere",      "func" : sphere,      "npop" :  6 * n, "nchi" : 6 * n},
-	{"name" : "k-tablet",    "func" : ktablet,     "npop" : 10 * n, "nchi" : 6 * n},
-	{"name" : "bohachevsky", "func" : bohachevsky, "npop" :  8 * n, "nchi" : 6 * n},
-	{"name" : "ackley",      "func" : ackley,      "npop" :  8 * n, "nchi" : 6 * n},
-	{"name" : "schaffer",    "func" : schaffer,    "npop" : 11 * n, "nchi" : 8 * n},
-	{"name" : "rastrigin",   "func" : rastrigin,   "npop" : 24 * n, "nchi" : 8 * n},
+	# {"name" : "k-tablet",    "func" : ktablet,     "npop" : 10 * n, "nchi" : 6 * n},
+	# {"name" : "bohachevsky", "func" : bohachevsky, "npop" :  8 * n, "nchi" : 6 * n},
+	# {"name" : "ackley",      "func" : ackley,      "npop" :  8 * n, "nchi" : 6 * n},
+	# {"name" : "schaffer",    "func" : schaffer,    "npop" : 11 * n, "nchi" : 8 * n},
+	# {"name" : "rastrigin",   "func" : rastrigin,   "npop" : 24 * n, "nchi" : 8 * n},
 ]
 
 datestr = "{0:%Y-%m-%d_%H-%M-%S}".format(datetime.datetime.now())
@@ -60,8 +60,8 @@ for problem in problems:
 	npar = n + 1
 	nchi = problem["nchi"]
 	eval_counts = {}
-	max_eval_count = 300000
-	loop_count = 100
+	max_eval_count = 4000
+	loop_count = 10
 
 	print(name, loop_count, flush = True)
 
@@ -72,77 +72,21 @@ for problem in problems:
 		bgg.select_for_reproduction = bgg.select_for_reproduction_partitioned
 		bgg.barometer = lambda: 1
 		result = bgg.until(1e-7, max_eval_count)
-		save(bgg, result, method_name, name)
-
-		method_name = "序盤は集団がランダムな3n(1200まで)"
-		ep = RestrictedPopulation(n, 3 * n, npar, 2 * n, npop, npar, nchi, func)
-		ep.should_expand = ep.is_over_deadline
-		ep.deadline = 1200
-		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
-
-		method_name = "序盤は集団がランダムな3n(t=1e-1)"
-		ep = RestrictedPopulation(n, 3 * n, npar, 2 * n, npop, npar, nchi, func)
-		ep.should_expand = ep.is_stucked
-		ep.t = 1e-1
-		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
+		save(bgg, result, method_name, name, i)
 
 		method_name = "序盤は集団がランダムな3n(t=1e-2)"
 		ep = RestrictedPopulation(n, 3 * n, npar, 2 * n, npop, npar, nchi, func)
 		ep.should_expand = ep.is_stucked
 		ep.t = 1e-2
 		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
-
-		method_name = "序盤は集団がランダムな3n(t=1e-3)"
-		ep = RestrictedPopulation(n, 3 * n, npar, 2 * n, npop, npar, nchi, func)
-		ep.should_expand = ep.is_stucked
-		ep.t = 1e-3
-		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
-
-		method_name = "序盤は集団がランダムな3n(t=1e-4)"
-		ep = RestrictedPopulation(n, 3 * n, npar, 2 * n, npop, npar, nchi, func)
-		ep.should_expand = ep.is_stucked
-		ep.t = 1e-4
-		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
-
-		method_name = "序盤は集団がランダムな5n(1200まで)"
-		ep = RestrictedPopulation(n, 5 * n, npar, 2 * n, npop, npar, nchi, func)
-		ep.should_expand = ep.is_over_deadline
-		ep.deadline = 1200
-		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
-
-		method_name = "序盤は集団がランダムな5n(t=1e-1)"
-		ep = RestrictedPopulation(n, 5 * n, npar, 2 * n, npop, npar, nchi, func)
-		ep.should_expand = ep.is_stucked
-		ep.t = 1e-1
-		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
+		save(ep, result, method_name, name, i)
 
 		method_name = "序盤は集団がランダムな5n(t=1e-2)"
 		ep = RestrictedPopulation(n, 5 * n, npar, 2 * n, npop, npar, nchi, func)
 		ep.should_expand = ep.is_stucked
 		ep.t = 1e-2
 		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
-
-		method_name = "序盤は集団がランダムな5n(t=1e-3)"
-		ep = RestrictedPopulation(n, 5 * n, npar, 2 * n, npop, npar, nchi, func)
-		ep.should_expand = ep.is_stucked
-		ep.t = 1e-3
-		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
-
-		method_name = "序盤は集団がランダムな5n(t=1e-4)"
-		ep = RestrictedPopulation(n, 5 * n, npar, 2 * n, npop, npar, nchi, func)
-		ep.should_expand = ep.is_stucked
-		ep.t = 1e-4
-		result = ep.until(1e-7, max_eval_count)
-		save(ep, result, method_name, name)
+		save(ep, result, method_name, name, i)
 
 	for method_name, best_fitness in eval_counts.items():
 		print(
--- plot.py
+++ plot.py
@@ -3,6 +3,8 @@ import argparse
 import csv
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+from scipy import stats
+import numpy as np
 
 font = {"family": "Noto Sans MONO CJK JP"}
 mpl.rc('font', **font)
@@ -12,6 +14,7 @@ def plot(filenames, ylabel, log_scaled = False):
 	if isinstance(filenames, str):
 		filenames = [filenames]
 
+	datas = {}
 	for filename in filenames:
 		with open(filename, "r") as f:
 			x = []
@@ -20,7 +23,45 @@ def plot(filenames, ylabel, log_scaled = False):
 			for row in reader:
 				x.append(row[0])
 				y.append(row[1])
-			plt.plot(x, y, linewidth = 0.5, label = filename.split("\\")[-1].replace(".csv", ""))
+			base_file_name = filename.split("\\")[-1].replace(".csv", "")
+			method_name = "_".join(base_file_name.split("_")[:-1])
+			index = int(base_file_name.split("_")[-1])
+			if not method_name in datas:
+				datas[method_name] = {}
+			datas[method_name][index] = {}
+			datas[method_name][index]["x"] = list(map(lambda i: int(i), x))
+			datas[method_name][index]["y"] = y
+
+	for method_name, data in datas.items():
+		data["raw_datas"] = {}
+		for i, d in data.items():
+			if i == "raw_datas":
+				continue
+			for xindex in data[0]["x"]:
+				if not xindex in data["raw_datas"]:
+					data["raw_datas"][xindex] = []
+				if index in data["raw_datas"]:
+					yindex = d["x"].index(int(xindex))
+					data["raw_datas"][xindex].append(d["y"][yindex])
+				else:
+					# Linear interpolation
+					near_pin_index = np.abs(np.asarray(d["x"]) - xindex).argsort()[:2]
+					a = d["y"][near_pin_index[0]]
+					b = d["y"][near_pin_index[1]]
+					i_a = d["x"][near_pin_index[0]]
+					i_b = d["x"][near_pin_index[1]]
+					data["raw_datas"][xindex].append(
+						a + (b - a) / (i_b - i_a) * (xindex - i_a))
+		data["means"] = []
+		data["sems"] = []
+		for xindex, d in data["raw_datas"].items():
+			data["means"].append(np.mean(d))
+			data["sems"].append(stats.sem(d))
+		plt.errorbar(
+			data[0]["x"],
+			data["means"],
+			yerr = data["sems"],
+			label = method_name)
 
 	if log_scaled:
 		plt.yscale("log")
